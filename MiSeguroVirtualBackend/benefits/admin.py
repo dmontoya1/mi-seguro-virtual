a0cc149d0e89f9b0797b36f4d84a7fbf4d9bf177
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2019-03-07 21:35:41 -0500

    Add benefits admin

--- MiSeguroVirtualBackend/benefits/admin.py
+++ MiSeguroVirtualBackend/benefits/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Benefits
+
+
+@admin.register(Benefits)
+class BenefitsAdmin(admin.ModelAdmin):
+    """
+    """
+
+    list_display = ('name', )
+    search_fields = ('name', 'reddeming_code')
